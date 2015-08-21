91898449ca08c6f8a19c79b663d1c5ccfa50cbf9
kxingit <kxin@me.com>
2015-08-20 22:46:56 -0500

    add Moore vote comment

--- Majority_Element_II.cpp
+++ Majority_Element_II.cpp
@@ -1,6 +1,7 @@
 /* 
  * Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. 
  * The algorithm should run in linear time and in O(1) space.
+ * https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
  */
 class Solution {
   public:
