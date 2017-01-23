a5d3cec2803b9b0a15e338953e16d643ee1d2685
Kefeng Xin <kefengx@hud12.dugeo.com>
2017-01-23 10:01:34 -0600

    Two-sum update

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -17,3 +17,23 @@ class Solution {
       return result;
     }
 };
+// index change
+class Solution {
+  public:
+	vector<int> twoSum(vector<int>& numbers, int target) {
+	  unordered_map<int, int> mapping;
+	  vector<int> result;
+	  for (int i = 0; i < numbers.size(); i++){
+		mapping[numbers[i]] = i;
+	  }
+	  for (int i = 0; i < numbers.size(); i++){
+		const int gap = target - numbers[i];
+		if(mapping.find(gap) != mapping.end() && mapping[gap] > i){
+		  result.push_back(i);
+		  result.push_back(mapping[gap]);
+		  break;
+		}
+	  }
+	  return result;
+	}
+};
