7cdbf07841a077801f65045fe74155aa3ef8695a
kxin@me.com <kxin@me.com>
2017-01-23 13:29:33 -0600

    update 2

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -37,3 +37,21 @@ class Solution {
 	  return result;
 	}
 };
+// optimization
+class Solution {
+  public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+	  vector<int> result;
+	  unordered_map<int, int> map;
+	  for(int i = 0; i < nums.size(); i++) {
+		int gap = target - nums[i];
+		if(map.find(gap) != map.end()) {
+		  result.push_back(i);
+		  result.push_back(map[gap]);
+		  break;
+		}
+		map[nums[i]] = i;
+	  }
+	  return result;
+	}
+};
