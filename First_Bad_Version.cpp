c660cf5606ac229b08a5728d2af42f7825f775db
kxingit <kxin@me.com>
2017-01-24 20:40:54 -0600

    End of day commit

--- First_Bad_Version.cpp
+++ First_Bad_Version.cpp
@@ -34,7 +34,24 @@ class Solution {
 	  start = mid;
 	}
       }
-      if(isBadVersion(start)) return start;
-      else return end;
-    }
+	  if(isBadVersion(start)) return start;
+	  else return end;
+	}
+};
+
+// v3
+class Solution {
+  public:
+	int firstBadVersion(int n) {
+	  int start = 1, end = n;
+	  while (start + 1 < end) {
+		int mid = start + (end - start) / 2;
+		if(isBadVersion(mid)) {
+		  end = mid;
+		} else {
+		  start = mid;
+		}
+	  }
+	  return isBadVersion(start) ? start : end;
+	}
 };
